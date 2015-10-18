16b31f1a32e4ca13b58851a834a3c63ba533b982
MichaelLSmith <mlsmith45@gmail.com>
2015-10-18 14:26:32 -0400

    deleted full js files

--- js/insultCtrl.js
+++
@@ -1,9 +0,0 @@
-function InsultCtrl(insultService) {
-    this.insultService = insultService;
-}
-
-InsultCtrl.prototype.replaceInsult = function (){
-    this.insult = this.insultService.generateInsult();
-}
-
-app.controller('InsultCtrl', InsultCtrl);
